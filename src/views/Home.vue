1c6e63358e4bcbaa12c048782f2ad7ea7d241fb4
julianv8 <juliandjv@gmail.com>
2021-07-28 21:03:52 -0500

    Correccion ortografia

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
-    <p>Esta prueba tecnica fue desarrollada en VueJS en forma de single page aplication</p>
-    <h5>Puede navegar atravez del menu para poder visualisar todos los retos</h5>
+    <p>Ésta prueba técnica fue desarrollada en VueJS en forma de <i>single page application</i></p>
+    <h5>Puede navegar a través del menú para poder visualizar todos los retos</h5>
     <h5>Julian Vasquez</h5>
   </div>
 </template>
